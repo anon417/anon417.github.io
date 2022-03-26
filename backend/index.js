e9d38c46db203aee25738d7bdced33a8bc3164e2
Serena Yung <serenayung2005@gmail.com>
2022-03-26 16:55:08 -0400

    pls

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ require('dotenv').config()
 
 const app = express()
 app.use(cors())
-const port = process.env.PORT || 5000;
+const port = 4000;
 
-const raw = fs.readFileSync('./data.csv').toString().split("\n")
+const raw = fs.readFileSync('data.csv').toString().split("\n")
 let data = []
 
 raw.map((line) => {
